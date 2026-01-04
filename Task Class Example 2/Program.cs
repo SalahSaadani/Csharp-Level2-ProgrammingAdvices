7f463fcd60014dd2d8a31c11986c1695ff9e9aad
Salah_Eddine_Saadani <salaheddinesaadani@gmail.com>
2026-01-04 21:18:23 +0100

    Add files via upload

---
+++ Task Class Example 2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+     
+        Console.WriteLine("Starting tasks...");
+
+        // Start the first task to download and print content length from CNN
+        Task task1 = DownloadAndPrintAsync("https://www.cnn.com");
+        Console.WriteLine("Task 1 started...");
+
+        // Start the second task to download and print content length from Amazon
+        Task task2 = DownloadAndPrintAsync("https://www.amazon.com");
+        Console.WriteLine("Task 2 started...");
+
+        // Start the third task to download and print content length from ProgrammingAdvices
+        Task task3 = DownloadAndPrintAsync("https://www.ProgrammingAdvices.com");
+        Console.WriteLine("Task 3 started...\n");
+
+        // Wait for all tasks to complete
+        await Task.WhenAll(task1, task2, task3);
+
+        // Print a message indicating that all tasks have finished execution
+        Console.WriteLine("\nDone, all tasks finished execution.");
+        Console.ReadKey();
+    }
+
+    static async Task DownloadAndPrintAsync(string url)
+    {
+       
+        string content;
+
+        // Using statement ensures that the WebClient is disposed of properly
+        using (WebClient client = new WebClient())
+        {
+            client.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
+
+            // Simulate some work by adding a delay
+            await Task.Delay(100);
+
+            // Download the content of the web page asynchronously
+            content = await client.DownloadStringTaskAsync(url);
+        }
+
+        // Print the URL and the length of the downloaded content
+        Console.WriteLine($"{url}: {content.Length} characters downloaded");
+    }
+}
