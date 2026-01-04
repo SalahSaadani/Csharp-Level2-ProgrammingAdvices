2102bb7f54feb01ccbfcde6f8ba934e5f2650544
Salah_Eddine_Saadani <salaheddinesaadani@gmail.com>
2026-01-04 21:13:02 +0100

    Add files via upload

---
+++ SimpleEventWithPrameters/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleEventWithPrameters
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void myUserControl1_OnCalculationComplete_1(int obj)
+        {
+            int Results = obj;
+            MessageBox.Show("Results =" + obj);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void myUserControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
