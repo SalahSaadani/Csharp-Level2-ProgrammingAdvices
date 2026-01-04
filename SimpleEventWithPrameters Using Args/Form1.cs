3692daccbcdd11f4cc5186169a43609ddbd38628
Salah_Eddine_Saadani <salaheddinesaadani@gmail.com>
2026-01-04 21:15:26 +0100

    Add files via upload

---
+++ SimpleEventWithPrameters Using Args/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleEventWithPrameters
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void myUserControl1_OnCalculationComplete(object sender, MyUserControl.CalculationCompleteEventArgs e)
+        {
+            
+            
+            MessageBox.Show ($"Results={e.Results} , val1 = {e.Val1}, vale2 = {e.Val2}" );
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SimpleEventWithPrameters Using Args/MyUserControl.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleEventWithPrameters
+{
+    public partial class MyUserControl : UserControl
+    {
+
+        //this is the old code....commented.
+        //// Define a custom event handler delegate with parameters
+        //public event Action<int> OnCalculationComplete;
+        //// Create a protected method to raise the event with a parameter
+        //protected virtual void CalculationComplete(int PersonID)
+        //{
+        //    Action<int> handler = OnCalculationComplete;
+        //    if (handler != null)
+        //    {
+        //        handler(PersonID); // Raise the event with the parameter
+        //    }
+        //}
+
+
+
+        public class CalculationCompleteEventArgs : EventArgs
+        {
+            public int Results { get; }
+            public int Val1 { get; }
+            public int Val2 { get; }
+
+            public CalculationCompleteEventArgs(int Results, int Val1, int Val2)
+            {
+                this.Results = Results;
+                this.Val1 = Val1;
+                this.Val2 = Val2;
+            }
+        }
+
+        public event EventHandler<CalculationCompleteEventArgs> OnCalculationComplete;
+
+        public void RaiseOnCalulationComplete(int Results, int Val1, int Val2)
+        {
+            RaiseOnCalulationComplete(new CalculationCompleteEventArgs(Results, Val1, Val2));
+        }
+
+        protected virtual void RaiseOnCalulationComplete(CalculationCompleteEventArgs e)
+        {
+            OnCalculationComplete?.Invoke(this, e);
+        }
+
+
+
+
+        public MyUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            
+            int Val1,Val2;
+            Val1 = Convert.ToInt32(txtNumber1.Text);
+            Val2 = Convert.ToInt32(txtNumber2.Text);
+
+            int Result = Val1  + Val2;
+            lblResult.Text = Result.ToString();
+
+            if (OnCalculationComplete != null)
+                // Raise the event with a parameter
+                RaiseOnCalulationComplete(Result, Val1, Val2);
+
+
+        }
+    }
+}
