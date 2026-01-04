aa0715c69627b9a4eed817470846b6b0b55b9e14
Salah_Eddine_Saadani <salaheddinesaadani@gmail.com>
2026-01-04 20:30:37 +0100

    Add files via upload

---
+++ Action Delegate Example/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Declare and initialize Action delegates with regular methods
+
+        Action parameterlessAction = ParameterlessMethod;
+        Action<int> actionWithIntParameter = ActionWithIntParameterMethod;
+        Action<string, int> actionWithMultipleParameters = ActionWithMultipleParametersMethod; 
+
+        // Invoking the actions
+
+        parameterlessAction();
+        actionWithIntParameter(42);
+        actionWithMultipleParameters("Hello, World!", 100);
+        Console.ReadKey();
+
+    }
+
+    // Regular methods to be used with Action delegates
+
+    static void ParameterlessMethod()
+    {
+        Console.WriteLine("This is a parameterless action.");
+    }
+
+    static void ActionWithIntParameterMethod(int x)
+    {
+        Console.WriteLine($"Action with int parameter: {x}");
+    }
+
+    static void ActionWithMultipleParametersMethod(string str, int num)
+    {
+        Console.WriteLine($"Action with string and int parameters: {str}, {num}");
+    }
+}
