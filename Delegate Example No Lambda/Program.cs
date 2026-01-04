5c997d274c3dd0ee9319bfc02f8e58c77f2c1040
Salah_Eddine_Saadani <salaheddinesaadani@gmail.com>
2026-01-04 20:37:58 +0100

    Add files via upload

---
+++ Delegate Example No Lambda/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    // A delegate that represents an operation
+    delegate int Operation(int x, int y);
+
+    // A function that takes a delegate with parameters and invokes it
+    static void ExecuteOperation(int x,int y, Operation operation)
+    {
+        int result = operation(x, y); // Invoke the provided delegate
+        Console.WriteLine("Result: " + result);
+    }
+
+    // A method that performs addition
+    static int Add(int x, int y)
+    {
+        return x + y;
+    }
+
+    // A method that performs subtraction
+    static int Sub(int x, int y)
+    {
+        return x - y;
+    }
+   
+    static void Main()
+    {
+        // Use the Add method with the delegate
+        Operation AddOp = Add;
+        Operation SubOp = Sub;
+
+        ExecuteOperation(10,20,AddOp); // Pass the delegate as an argument
+        ExecuteOperation(10, 20, SubOp); // Pass the delegate as an argument
+
+     
+        Console.ReadLine();
+
+    }
+}
