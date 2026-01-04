ac144fe7a5a2beb80b1887aa8441b7396c0bac1b
Salah_Eddine_Saadani <salaheddinesaadani@gmail.com>
2026-01-04 21:04:19 +0100

    Add files via upload

---
+++ Reading from the Registry/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Win32;
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        // Specify the Registry key and path
+       // string keyPath = @"HKEY_LOCAL_MACHINE\SOFTWARE\YourSoftware";
+        string keyPath = @"HKEY_CURRENT_USER\SOFTWARE\YourSoftware";
+        string valueName = "YourValueName";
+      
+        try
+        {
+            // Read the value from the Registry
+            string value = Registry.GetValue(keyPath, valueName, null) as string;
+
+
+            if (value != null)
+            {
+                Console.WriteLine($"The value of {valueName} is: {value}");
+            }
+            else
+            {
+                Console.WriteLine($"Value {valueName} not found in the Registry.");
+            }
+            Console.ReadKey();
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+}
